bc5ff8647626b3355987d5cb746cc55b9ac4124f
Vipin <vicky7230@gmail.com>
2017-06-03 15:12:43 +0530

    sign out added

--- app/src/main/java/com/yoscholar/ping/activity/SettingsActivity.java
+++ app/src/main/java/com/yoscholar/ping/activity/SettingsActivity.java
@@ -1,18 +1,24 @@
 package com.yoscholar.ping.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.joanzapata.iconify.widget.IconTextView;
 import com.yoscholar.ping.BuildConfig;
 import com.yoscholar.ping.R;
+import com.yoscholar.ping.utils.AppPreference;
 
 public class SettingsActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
     private TextView versionInfoTextView;
+    private IconTextView signOutTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +37,21 @@ public class SettingsActivity extends AppCompatActivity {
 
         versionInfoTextView = (TextView) findViewById(R.id.version_info);
         versionInfoTextView.setText("version : " + BuildConfig.VERSION_NAME);
+
+        signOutTextView = (IconTextView) findViewById(R.id.sign_out);
+        signOutTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                AppPreference.logOut(SettingsActivity.this);
+
+                Toast.makeText(SettingsActivity.this, "Signed Out", Toast.LENGTH_SHORT).show();
+
+                openLoginScreen();
+
+                finish();
+            }
+        });
     }
 
 
@@ -48,4 +69,13 @@ public class SettingsActivity extends AppCompatActivity {
 
         }
     }
+
+    private void openLoginScreen() {
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+
+    }
+
 }
